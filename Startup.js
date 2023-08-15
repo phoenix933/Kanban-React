4c96244036f206e750a8cc0e07def2fdcb95801d
crashdev226 <midashand44@gmail.com>
2023-08-15 14:39:42 +0900

    Update Startup.js
    Co-Authored-By: LouisWinkler <138194633+LouisWinkler@users.noreply.github.com>

--- Startup.js
+++ Startup.js
@@ -5,33 +5,6 @@ const StartUp = () => {
     return (
         <>
             <div className="flex flex-col justify-center items-center gap-4 flex-wrap">
-                <div className="flex justify-center items-center w-40 h-40">
-                    16
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    17
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    18
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    19
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    20
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    21
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    22
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    23
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    24
-                </div>
             </div>
         </>
     )
