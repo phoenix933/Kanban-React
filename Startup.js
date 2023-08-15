499cf3667b36846b698b7e6278d4ef869f80a109
crashdev226 <midashand44@gmail.com>
2023-08-15 14:33:17 +0900

    Update Startup.js
    Co-Authored-By: LouisWinkler <138194633+LouisWinkler@users.noreply.github.com>

--- Startup.js
+++ Startup.js
@@ -6,49 +6,7 @@ const StartUp = () => {
         <>
             <div className="flex flex-col justify-center items-center gap-4 flex-wrap">
                 <div className="flex justify-center items-center w-40 h-40">
-                    1
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    2
-                </div>                
-                <div className="flex justify-center items-center w-40 h-40">
-                    3
-                </div>                
-                <div className="flex justify-center items-center w-40 h-40">
-                    4
-                </div>                
-                <div className="flex justify-center items-center w-40 h-40">
-                    5
-                </div>                
-                <div className="flex justify-center items-center w-40 h-40">
-                    6
-                </div>                
-                <div className="flex justify-center items-center w-40 h-40">
-                    7
-                </div>                
-                <div className="flex justify-center items-center w-40 h-40">
-                    8
-                </div>                
-                <div className="flex justify-center items-center w-40 h-40">
-                    9
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    10
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    11
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    12
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    13
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    14
-                </div>
-                <div className="flex justify-center items-center w-40 h-40">
-                    15
+                    16
                 </div>
             </div>
         </>
