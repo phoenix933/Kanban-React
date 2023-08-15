069b997cb0d8831e4fd5976b21173012fd8343e5
crashdev226 <midashand44@gmail.com>
2023-08-15 14:29:43 +0900

    Update Startup.js
    Co-Authored-By: LouisWinkler <138194633+LouisWinkler@users.noreply.github.com>

--- Startup.js
+++ Startup.js
@@ -41,6 +41,9 @@ const StartUp = () => {
                 <div className="flex justify-center items-center w-40 h-40">
                     12
                 </div>
+                <div className="flex justify-center items-center w-40 h-40">
+                    13
+                </div>
             </div>
         </>
     )
